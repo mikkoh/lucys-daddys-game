5fbf2e16c953b5dde9f0098e499401b23698bcaa
Mikko Haapoja <me@mikkoh.com>
2016-02-09 22:42:12 -0500

    Added some assets and a quick test script to test dragging

--- test/dragItem.js
+++ test/dragItem.js
@@ -2,23 +2,38 @@ var React = require('react');
 var ReactDom = require('react-dom');
 var DragItem = require('../lib/components/DragItem');
 
+document.body.style.backgroundColor = '#f2f6ed';
+
 ReactDom.render(
-  <DragItem
-    position={[ 100, 100 ]}
-    onStart={handleTouch.bind(null, 'start')}
-    onMove={handleTouch.bind(null, 'move')}
-    onEnd={handleTouch.bind(null, 'end')}
-  >
-    <div 
-      style={{
-        width: 100,
-        height: 100,
-        backgroundColor: '#CAFE00'
-      }}
-    />
-  </DragItem>,
+  <div>
+    {
+      [
+        getDragItem('mom.png', 383, 41),
+        getDragItem('dad.png', 685, 45),
+        getDragItem('baby.png', 135, 148)
+      ]
+    }
+  </div>,
   document.body.appendChild(document.createElement('div'))
 );
+
+function getDragItem(url, x, y) {
+  this.key = this.key || 0;
+  this.key++;
+
+  return <DragItem
+      key={this.key}
+      position={[ x || 0, y || 0 ]}
+      onStart={handleTouch.bind(null, 'start')}
+      onMove={handleTouch.bind(null, 'move')}
+      onEnd={handleTouch.bind(null, 'end')}
+    >
+      <img 
+        src={url}
+        draggable={false}
+      />
+    </DragItem>;
+}
 
 function handleTouch(type, pos) {
   console.log(type, pos);
